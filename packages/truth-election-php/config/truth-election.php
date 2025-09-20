fc8324a7ea1e36ecb0b853fc10650bddf46ebf07
Lester Hurtado <lester@lyflyn.net>
2025-09-21 07:24:13 +0800

    add new pipes re pesisting of er and er artifacts

--- packages/truth-election-php/config/truth-election.php
+++ packages/truth-election-php/config/truth-election.php
@@ -1,10 +1,17 @@
 <?php
 
+use TruthElection\Pipes\PersistElectionReturnJson;
+use TruthElection\Pipes\EncodeElectionReturnLines;
+
 return [
     'finalize_election_return' => [
         'pipes' => [
-            // any custom pipe
+            PersistElectionReturnJson::class,
+            EncodeElectionReturnLines::class
         ],
+    ],
+    'storage' => [
+        'disk' => env('TRUTH_ELECTION_DISK', 'local'),
     ],
 //    'store' => \TruthElection\Support\InMemoryElectionStore::class,
 ];
